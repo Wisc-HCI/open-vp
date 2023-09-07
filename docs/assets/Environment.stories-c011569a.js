94f4cbdd830ca97a446344d865307d332bfd8a28
Andrew Schoen <schoen.andrewj@gmail.com>
2023-09-06 22:54:13 -0500

    v0.8.13

---
+++ docs/assets/Environment.stories-c011569a.js
@@ -0,0 +1,51 @@
+import{r as w,j as e}from"./isSymbol-fd368dfe.js";import{E as p,u as D,a as d,b as T,c as E}from"./ErrorFallback-29d2f313.js";import{b as v,a as x}from"./basicStarter-d0729970.js";import"./_commonjsHelpers-042e6b4d.js";import"./assertThisInitialized-f2aa7f15.js";import"./index-58d3fd43.js";import"./debounce-c4e1af20.js";import"./Synchronizing-a5232913.js";const H={title:"Fullscreen",component:p},y=`
+> [error] There was an error!
+
+> [warn] There was an warning related to [The Program](programType)!
+
+> [info] Here is some info!
+
+> [success] Here is a success message!
+
+> Here is a block quote!
+`,S=f=>{const{drawers:a,objectTypes:t,programData:o,tabs:s,activeTab:n,executionData:i,drawerWidth:g,...h}=f,[b,c]=D();return w.useEffect(()=>{d.setState({programSpec:{drawers:a,objectTypes:t},programData:o,executionData:i,featuredDocs:{"2dfsessfs":y},tabs:s,activeTab:n})},[a,t,o,i,s,n]),e(T,{FallbackComponent:E,children:e("div",{ref:b,style:{display:"flex",height:"100vh",flexDirection:"row",backgroundColor:"#333"},children:e(p,{...h,store:d,height:c.height,width:c.width,drawerWidth:g})})})},r=S.bind({});r.args={highlightColor:"#ff00ff",drawerWidth:235,snapToGrid:!0,...v,...x};var u,m,l;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`args => {
+  const {
+    drawers,
+    objectTypes,
+    programData,
+    tabs,
+    activeTab,
+    executionData,
+    drawerWidth,
+    ...otherArgs
+  } = args;
+  const [ref, bounds] = useMeasure();
+  useEffect(() => {
+    useDefaultProgrammingStore.setState({
+      programSpec: {
+        drawers,
+        objectTypes
+      },
+      programData,
+      executionData,
+      featuredDocs: {
+        "2dfsessfs": featured
+        // "45535153s":true
+      },
+
+      tabs,
+      activeTab
+    });
+  }, [drawers, objectTypes, programData, executionData, tabs, activeTab]);
+  return <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <div ref={ref} style={{
+      display: "flex",
+      height: "100vh",
+      flexDirection: "row",
+      backgroundColor: "#333"
+    }}>
+        <Environment {...otherArgs} store={useDefaultProgrammingStore} height={bounds.height} width={bounds.width} drawerWidth={drawerWidth} />
+      </div>
+    </ErrorBoundary>;
+}`,...(l=(m=r.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const _=["Fullscreen"];export{r as Fullscreen,_ as __namedExportsOrder,H as default};
+//# sourceMappingURL=Environment.stories-c011569a.js.map
---
+++ docs/assets/External.stories-5b85ef74.js
@@ -0,0 +1,37 @@
+import{j as r,r as m,a as h}from"./isSymbol-fd368dfe.js";import{p as u,P as f,q as y,r as w,s as x,t as b,v as E,w as S,V as B,a as i,x as k,b as v,c as D}from"./ErrorFallback-29d2f313.js";import{b as P,a as T}from"./basicStarter-d0729970.js";import"./_commonjsHelpers-042e6b4d.js";import"./assertThisInitialized-f2aa7f15.js";import"./index-58d3fd43.js";import"./debounce-c4e1af20.js";import"./Synchronizing-a5232913.js";const C=t=>{const[e,a]=b(m.useCallback(n=>S(n,t.data,null),[t.data]),E);return r(B,{...t,...{x:0,z:0,scale:1,onCanvas:!1,interactionDisabled:!0,bounded:!0,fieldInfo:"outside",parentId:"outside"},data:e,typeSpec:a})},j=({highlightColor:t,data:e,style:a,context:o})=>r(y,{backend:w,options:x,children:r(C,{highlightColor:t,data:e,style:{...a},context:o})}),l=({store:t,highlightColor:e,data:a,style:o,context:n})=>r(u,{highlightColor:e,children:r(f,{store:t,children:r(j,{highlightColor:e,data:a,style:{...o},context:n})})}),U={title:"External",component:l},F=t=>{const{drawers:e,objectTypes:a,programData:o,highlightColor:n}=t;m.useEffect(()=>{i.setState({programSpec:{drawers:e,objectTypes:a},programData:o})},[e,a,o]);const g=k("hatType",o["6dewwwwww"],a.hatType);return r(v,{FallbackComponent:D,children:h("div",{style:{width:300,padding:4},children:[r("div",{style:{paddingBottom:4},children:r(l,{store:i,data:o["2dfsessfs"],highlightColor:n,context:{}})}),r("div",{style:{paddingBottom:4},children:r(l,{store:i,data:g,highlightColor:n,context:{}})})]})})},s=F.bind({});s.args={highlightColor:"#ff00ff",drawerWidth:235,...P,...T};var d,c,p;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`args => {
+  const {
+    drawers,
+    objectTypes,
+    programData,
+    highlightColor
+  } = args;
+  useEffect(() => {
+    useDefaultProgrammingStore.setState({
+      programSpec: {
+        drawers,
+        objectTypes
+      },
+      programData
+    });
+  }, [drawers, objectTypes, programData]);
+  const instRef = referenceTemplateFromSpec('hatType', programData['6dewwwwww'], objectTypes.hatType);
+  return <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <div style={{
+      width: 300,
+      padding: 4
+    }}>
+      <div style={{
+        paddingBottom: 4
+      }}>
+        <ExternalBlock store={useDefaultProgrammingStore} data={programData['2dfsessfs']} highlightColor={highlightColor} context={{}} />
+      </div>
+      <div style={{
+        paddingBottom: 4
+      }}>
+        <ExternalBlock store={useDefaultProgrammingStore} data={instRef} highlightColor={highlightColor} context={{}} />
+      </div>
+      
+    </div>
+    </ErrorBoundary>;
+}`,...(p=(c=s.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const A=["External"];export{s as External,A as __namedExportsOrder,U as default};
+//# sourceMappingURL=External.stories-5b85ef74.js.map
